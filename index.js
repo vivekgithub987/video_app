7081f68b4c922b8c5337800ad48ae1fbcc032917
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2025-02-11 04:49:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
+const port = process.env.PORT ?? 5000;
+
+const rooms = [];
+
+io.on("connection", (socket) => {
+  socket.on("join room", (roomId) => {
+    if (rooms[roomId]) {
+      rooms[roomId].push(socket.id);
+    } else {
+      rooms[roomId] = [socket.id];
+    }
+    const otherUser = rooms[roomId].find((id) => id !== socket.id);
+    if (otherUser) {
+      socket.emit("other user", otherUser);
+      socket.to(otherUser).emit("user joined", socket.id);
+    }
+  });
+
+  socket.on("offer", (payload) => {
+    io.to(payload.target).emit("offer", payload);
+  });
+
+  socket.on("answer", (payload) => {
+    io.to(payload.target).emit("answer", payload);
+  });
+
+  socket.on("ice-candidate", (incoming) => {
+    io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+  });
+});
+
+if (process.env.PROD) {
+  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
+server.listen(port, () =>
+  console.log("server is runing on http://localhost:" + port)
+);
