36ced2e69a7e96831b12cae65272b44d6eb61af9
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2025-02-11 08:41:01 +0530

    feature: screen sharing

--- client/src/routes/Room.jsx
+++ client/src/routes/Room.jsx
@@ -8,6 +8,7 @@ const Room = (props) => {
   const socketRef = React.useRef();
   const otherUser = React.useRef();
   const userStream = React.useRef();
+  const senders = React.useRef([]);
 
   React.useEffect(() => {
     navigator.mediaDevices
@@ -34,11 +35,25 @@ const Room = (props) => {
       });
   }, []);
 
+  React.useEffect(() => {
+    document.addEventListener("visibilitychange", (event) => {
+      if (document.visibilityState === "visible") {
+        if (document.pictureInPictureElement) {
+          document.exitPictureInPicture();
+        }
+      }
+    });
+  }, []);
+
   function callUser(userId) {
     peerRef.current = createPeer(userId);
     userStream.current
       .getTracks()
-      .forEach((track) => peerRef.current.addTrack(track, userStream.current));
+      .forEach((track) =>
+        senders.current.push(
+          peerRef.current.addTrack(track, userStream.current)
+        )
+      );
   }
 
   function createPeer(userId) {
@@ -46,6 +61,11 @@ const Room = (props) => {
       iceServers: [
         {
           urls: "stun:stun.stunprotocol.org",
+        },
+        {
+          url: "turn:numb.viagenie.ca",
+          credential: "muazkh",
+          username: "webrtc@live.com",
         },
       ],
     });
@@ -56,69 +76,107 @@ const Room = (props) => {
     return peer;
   }
 
-    function handleNegotiationNeededEvent(userId) {
-        peerRef.current.createOffer().then(offer => {
-            return peerRef.current.setLocalDescription(offer);
-        }).then(() => {
-            const payload = {
-                target: userId,
-                caller: socketRef.current.id,
-                sdp: peerRef.current.localDescription
-            };
-            socketRef.current.emit("offer", payload);
-        }).catch(e => console.log(e));
+  function handleNegotiationNeededEvent(userId) {
+    peerRef.current
+      .createOffer()
+      .then((offer) => {
+        return peerRef.current.setLocalDescription(offer);
+      })
+      .then(() => {
+        const payload = {
+          target: userId,
+          caller: socketRef.current.id,
+          sdp: peerRef.current.localDescription,
+        };
+        socketRef.current.emit("offer", payload);
+      })
+      .catch((e) => console.log(e));
+  }
+
+  function handleReceiveCall(incoming) {
+    peerRef.current = createPeer();
+    const desc = new RTCSessionDescription(incoming.sdp);
+    peerRef.current
+      .setRemoteDescription(desc)
+      .then(() => {
+        userStream.current
+          .getTracks()
+          .forEach((track) =>
+            peerRef.current.addTrack(track, userStream.current)
+          );
+      })
+      .then(() => {
+        return peerRef.current.createAnswer();
+      })
+      .then((answer) => {
+        return peerRef.current.setLocalDescription(answer);
+      })
+      .then(() => {
+        const payload = {
+          target: incoming.caller,
+          caller: socketRef.current.id,
+          sdp: peerRef.current.localDescription,
+        };
+        socketRef.current.emit("answer", payload);
+      });
+  }
+
+  function handleAnswer(message) {
+    const desc = new RTCSessionDescription(message.sdp);
+    peerRef.current.setRemoteDescription(desc).catch((e) => console.log(e));
+  }
+
+  function handleICECandidateEvent(e) {
+    if (e.candidate) {
+      const payload = {
+        target: otherUser.current,
+        candidate: e.candidate,
+      };
+      socketRef.current.emit("ice-candidate", payload);
     }
+  }
 
-    function handleReceiveCall(incoming) {
-        peerRef.current = createPeer();
-        const desc = new RTCSessionDescription(incoming.sdp);
-        peerRef.current.setRemoteDescription(desc).then(() => {
-            userStream.current.getTracks().forEach(track => peerRef.current.addTrack(track, userStream.current));
-        }).then(() => {
-            return peerRef.current.createAnswer();
-        }).then((answer) => {
-            return peerRef.current.setLocalDescription(answer);
-        }).then(()  => {
-            const payload = {
-                target: incoming.caller,
-                caller: socketRef.current.id,
-                sdp: peerRef.current.localDescription
-            }
-            socketRef.current.emit("answer", payload);
-        })
-    }
+  function handleNewICECandidateMsg(incoming) {
+    const candidate = new RTCIceCandidate(incoming);
 
-    console.log("Room is rendered");
+    peerRef.current.addIceCandidate(candidate).catch((e) => console.log(e));
+  }
 
-    function handleAnswer(message) {
-        const desc = new RTCSessionDescription(message.sdp);
-        peerRef.current.setRemoteDescription(desc).catch(e => console.log(e));
-    }
+  function handleTrackEvent(e) {
+    partnerVideo.current.srcObject = e.streams[0];
+  }
 
-    function handleICECandidateEvent(e) {
-        if(e.candidate){
-            const payload = {
-                target: otherUser.current,
-                candidate: e.candidate
-            }
-            socketRef.current.emit("ice-candidate", payload);
-        }
-    }
+  function requestPictureInPicture() {
+    partnerVideo.current.requestPictureInPicture();
+  }
 
-    function handleNewICECandidateMsg (incoming) {
-        const candidate =  new RTCIceCandidate(incoming);
+  function shareScreen() {
+    navigator.mediaDevices.getDisplayMedia({ cursor: true }).then((stream) => {
+      const screenTrack = stream.getTracks()[0];
+      senders.current
+        .find((sender) => sender.track.kind === "video")
+        .replaceTrack(screenTrack);
+      screenTrack.onended = function () {
+        senders.current
+          .find((sender) => sender.track.kind === "video")
+          .replaceTrack(userStream.current.getTracks()[1]);
+      };
+    });
+  }
 
-        peerRef.current.addIceCandidate(candidate).catch(e => console.log(e));
-    }
-
-    function handleTrackEvent(e){
-        partnerVideo.current.srcObject = e.streams[0];
-    }
+  function copyLink() {
+    navigator.clipboard.writeText(props.match.params.roomID);
+  }
 
   return (
     <div>
-      <video autoPlay ref={userVideo}></video>
-      <video autoPlay ref={partnerVideo}></video>
+      <video controls autoPlay ref={userVideo}></video>
+      <video controls autoPlay ref={partnerVideo}></video>
+      <button onClick={shareScreen}>Share Screen</button>
+      <button onClick={copyLink}>Copy Link</button>
+      <button onClick={requestPictureInPicture}>
+        Request picture in picture{" "}
+      </button>
     </div>
   );
 };
